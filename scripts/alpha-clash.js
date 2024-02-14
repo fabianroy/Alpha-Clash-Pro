fc5002f918a954ec986d503e2e4c0f03e7dc85bf
Fabian Roy <fabianroylikhon@gmail.com>
2024-02-15 00:33:28 +0600

    Huge Update

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,13 @@
+function continueGame(){
+    // step 1 : Generate a random alphabet.
+    let alphabet = getARandomAlphabet();
+    let currentAlphabet = document.getElementById('current-alphabet');
+    currentAlphabet.innerText = alphabet;
+    setBackgroundColorById(alphabet);
+}
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('playground-section');
+    continueGame();
+}
---
+++ scripts/utility.js
@@ -0,0 +1,25 @@
+function hideElementById(elelementId) {
+    let element = document.getElementById(elelementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elelementId) {
+    let element = document.getElementById(elelementId);
+    element.classList.remove('hidden');
+}
+
+function setBackgroundColorById(elelementId){
+    let element = document.getElementById(elelementId);
+    element.classList.add('bg-orange-400');
+}
+
+function getARandomAlphabet() {
+    let alphabetsString = "abcdefghijklmnopqrstuvwxyz";
+    let alphabets = alphabetsString.split('');
+
+    // get a random index between 0 to 25
+    let randomNumber = Math.random() * 25;
+    let index = Math.round(randomNumber);
+    let alphabet = alphabets[index];
+    return alphabet;
+}
